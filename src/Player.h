75dba18382cf48e91428a3d6164d4f5d23140a1e
YeTongY <yetongy77@gmail.com>
2025-11-15 15:15:19 +0800

    misc: 删去Player下的占位符

--- src/Player.h
+++ src/Player.h
@@ -1,32 +1,18 @@
-#ifndef PLAYER_H
-#define PLAYER_H
+#pragma once
 
-#include "raylib.h"
-
-// 玩家角色类
-class Player {
-private:
-    Vector2 position;
-    int hp;
-    int maxHp;
-    int attack;
-    int defense;
-    float speed;
-    
-public:
-    Player();
-    ~Player();
-    
-    void Update();
-    void Draw();
-    void Move(Vector2 direction);
-    void TakeDamage(int damage);
-    void Heal(int amount);
-    
-    // Getters
-    Vector2 GetPosition() const { return position; }
-    int GetHP() const { return hp; }
-    int GetMaxHP() const { return maxHp; }
+struct Stats {//声明玩家基础属性
+    int hp;//当前hp
+    int maxHp;//hp上限
+    int attack;//攻击力
+    int defense;//防御力
 };
 
-#endif // PLAYER_H
+struct Player {//声明玩家主结构体
+    int gridX; // 玩家在“网格”上的 X 坐标 (逻辑)
+    int gridY; // 玩家在“网格”上的 Y 坐标 (逻辑)
+    
+    Stats stats; // 玩家的属性
+    
+    // (P1 阶段)
+    // Texture2D playerTexture; // P1: 玩家的贴图
+};
