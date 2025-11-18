1c2a0509929e91ec95f242ca3177ddd8cc8faac9
YeTongY <yetongy77@gmail.com>
2025-11-18 11:50:05 +0800

    chore: 添加ui初始结构

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,4 +1,35 @@
 #include "UI.h"
+#include "GameState.h"
+
+struct DialogueBoxTemplate_Normal{//创建一般对话框模板
+    //对话框样式设置
+    Texture2D dialogueBoxTexture;
+    float dialogueBoxWidth = 1800.0f;
+    float dialogueBoxHeight = 280.0f;
+    Vector2 dialogueBoxPosition = {60.0f,760.0f};
+
+    //头像框样式设置
+    Texture2D potraitFrameTexture;
+    float potraitFrameWidth = 250.0f;
+    float potraitFrameHeight = 250.0f;
+    Vector2 potraitFramePosition = {100.0f,635.0f};
+
+    //头像样式设置
+    float potraitWidth = 210.0f;
+    float potraitHeight = 210.0f;
+    Vector2 potraitPosition = {120.0f,655.0f};
+
+    //字体样式设置
+    float fontSize = 48.0f;
+    //textColor 直接用Gamestate.h宏定义的UI_TEXT
+    //glowColor 直接用CYBER_CYAN
+    Vector2 textOffset = {330.0f, 50.0f};
+    Font font = nullptr;
+
+
+
+};
+
 
 void LoadUIAssets(GameContext& ctx){
     const char* dialogue_box_address = "../res/graphics/ui/dialogue_box.png";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           \
