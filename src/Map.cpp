0cbfcbbff93a9295ddd0c4434f45c501dfa1ddad
ChuangGe0315 <2729834195@qq.com>
2025-11-19 11:25:21 +0800

    feat:实现了敌人数据读取和ai逻辑移动，等待状态机和渲染部分调整

--- src/Map.cpp
+++ src/Map.cpp
@@ -319,7 +319,7 @@ bool LoadLevelFromTiled(GameContext& ctx, const char* filepath){
 
                 //设置其他默认属性
                 enemy.isActive = true;
-                enemy.isMoving = true;
+                enemy.isMoving = false;
                 enemy.aiState = AI_STATE_PATROL;
                 enemy.currentDirection = ENEMY_DIR_DOWN;
 
