59789986c683f024f77a366c4fd0c4b29a014d97
YeTongY <yetongy77@gmail.com>
2025-12-02 20:28:12 +0800

    feat:尽力了.....地图l0-1

---
+++ res/tsx/1_Generic_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="1_Generic_32x32" tilewidth="32" tileheight="32" tilecount="1248" columns="16">
+ <image source="../graphics/tilesets/1_Generic_32x32.png" width="512" height="2496"/>
+</tileset>
---
+++ res/tsx/Interiors_Part2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Interiors_Part2" tilewidth="32" tileheight="32" tilecount="4096" columns="16">
+ <image source="../graphics/tilesets/Interiors_Part2.png" width="512" height="8192"/>
+</tileset>
